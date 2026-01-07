6580de9d981663de516861f818f24a60154ee120
Jason Leong <jleongdh@gmail.com>
2026-01-07 23:59:03 +1030

    Added photo to landing page

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,31 +1,36 @@
-import React from 'react'
-import { Button } from '../ui/button.jsx'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Button } from "../ui/button.jsx";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <>
-      <div className='flex flex-col mx-56 gap-9 items-center h-screen'>
-        <h1 className='font-extrabold text-center text-[60px] mt-15'>
-          <span className='text-purple-500'>Discover Your Next Adventure with A</span><br /> 
-          AI Personalised intinary 
+      <div className="flex flex-col mx-56 gap-9 items-center h-screen">
+        <h1 className="font-extrabold text-center text-[60px] mt-15">
+          <span className="text-purple-500">
+            Discover Your Next Adventure with A
+          </span>
+          <br />
+          AI Personalised intinary
         </h1>
 
-        <p className='text-2xl text-gray-500 text-center'>
-          Your AI-Powered trip planner and travel curator, designed to create custom itineraries tailored to your interest and budget
+        <p className="text-2xl text-gray-500 text-center">
+          Your AI-Powered trip planner and travel curator, designed to create
+          custom itineraries tailored to your interest and budget
         </p>
 
-        <Link to={'/create-trip'}>
-          <Button>
-            Get Started. It's Free
-          </Button>
+        <Link to={"/create-trip"}>
+          <Button>Get Started. It's Free</Button>
         </Link>
 
-        
-      
+        <img
+          src="/landing.png"
+          className="-mt-20 shadow-md border-slate-600 rounded-md "
+          alt=""
+        />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
